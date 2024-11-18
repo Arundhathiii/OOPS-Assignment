7ae09c9fc60b11183af3ace73da623a27a9a9fa6
Arundhathi Ajay AS <arundhathiajay05@gmail.com>
2024-11-18 11:29:19 +0530

    Add files via upload

---
+++ (Assignment extension for OOPS).py
@@ -0,0 +1,15 @@
+"""
+Question 2: (5 Marks) Create a Python module named employee that contains a class Employee with attributes name,
+salary and methods get_name() and get_salary().
+"""
+# class Employee:
+#     def __init__(self, name, salary):
+#         self.name = name
+#         self.salary = salary
+#
+#     def get_name(self):
+#         return self.name
+#
+#     def get_salary(self):
+#         return self.salary
+
